74d73201530f5557b8ebec6164f3db7d2bfa4ab4
NguyenMinhTuan <tuannb0mmm.2x1y9z@gmail.com>
2023-02-08 17:53:07 +0700

    Update UI Header

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,6 +1,10 @@
 import classNames from 'classnames/bind';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -8,7 +12,30 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok"/>
+        </div>
+
+        <div className={cx('search')}>
+          <input
+            placeholder='Search accounts and video'
+            // Check chính tả
+            spellCheck={false}
+          />
+          <button className={ cx('clear-btn')}>
+            {/* Clear */}
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={ cx('search-btn')}>
+            {/* Search Icon */}
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
         
+        <div className={cx('action')}>
+
+        </div>
       </div>
     </header>
   );
