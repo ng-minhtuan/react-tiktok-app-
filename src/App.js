4c381fb5c8c903d356750144b4551fa70d3c1095
NguyenMinhTuan <tuannb0mmm.2x1y9z@gmail.com>
2023-02-08 14:57:03 +0700

    Pretty

--- src/App.js
+++ src/App.js
@@ -11,13 +11,17 @@ function App() {
         <Routes>
           {
             publicRoutes.map((route, index) => {
+
               let Layout = DefaultLayout;
+
               if (route.layout) {
                 Layout =route.layout;
               } else if (route.layout === null) {
                 Layout = Fragment;
               }
+
               const Page = route.component;
+              
               return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
             })
           }
